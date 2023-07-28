a364a384bbafbf56749e8ab6a67c5bfc1916c2ec
IbrahimTuzer <ibrahimtuzer12@gmail.com>
2023-07-29 01:00:35 +0300

    Controller dosyaları, Viewlar ve route tanımlamaları yapıldı. Toplantı 1-2 task 1'den devam edilecek

---
+++ Ads.Web.Mvc/Controllers/AdvertController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ads.Web.Mvc.Controllers
+{
+    public class AdvertController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Search(string query,int page)
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
+
--- Ads.Web.Mvc/Program.cs
+++ Ads.Web.Mvc/Program.cs
@@ -24,4 +24,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+        name: "Admin",
+        pattern: "{area:exists}/{controller}/{action}"
+    );
+
 app.Run();
