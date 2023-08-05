ec43743a4a61760e9536c205b6460171926c3335
IbrahimTuzer <ibrahimtuzer12@gmail.com>
2023-08-06 00:53:22 +0300

    .

---
+++ Ads.Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Ads.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Data
+{
+	public class AppDbContext: DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Advert> Adverts { get; set; }
+		public DbSet<CategoryAdvert> CategoryAdverts { get; set; }
+		public DbSet<AdvertImage> AdvertImages { get; set; }
+		public DbSet<AdvertComment> AdvertComments { get; set; }
+		public DbSet<Page> Pages { get; set; }
+		public DbSet<User> Users { get; set; }
+		public DbSet<Setting> Settings { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			// Örnek: modelBuilder.Entity<Category>().HasMany(c => c.Adverts).WithOne(a => a.Category);
+			// ...
+			DbSeeder.Seed(modelBuilder);
+			base.OnModelCreating(modelBuilder);
+
+		}
+	}
+}
--- Ads.Web.Mvc/Program.cs
+++ Ads.Web.Mvc/Program.cs
@@ -1,10 +1,28 @@
+﻿using Ads.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(o =>
+{
+	// appSettings.json içerisindeki Default baðlantý metnini almayý saðlar.
+	string connectionString = builder.Configuration.GetConnectionString("Default");
+	o.UseSqlServer(connectionString);
+});
 
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+	// Veritabaný servisine eriþim saðlar.
+	var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+	// Veritabanýný sil
+	//context.Database.EnsureDeleted();
+	// Veritabanýný oluþturur
+	context.Database.EnsureCreated();
+}
 
 
 // Configure the HTTP request pipeline.
