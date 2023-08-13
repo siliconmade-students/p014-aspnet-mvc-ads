a646278addc6309df5d67989a23c587c9eb0a5f2
Cem Gündüzoğlu <cmg.web@gmail.com>
2023-08-13 22:13:30 +0300

    Son düzeltmeler

--- Ads.Data/DbSeeder.cs
+++ Ads.Data/DbSeeder.cs
@@ -1,39 +1,35 @@
-﻿using Ads.Data.Entity;
-using Ads.Data;
-using Microsoft.EntityFrameworkCore;
+﻿using Ads.Data;
+using Ads.Data.Entity;
 
 
+public class DbSeeder
+{
+    public static void Seed(AppDbContext dbContext)
+    {
+        // Check if Pages already exist in the database
+        if (!dbContext.Pages.Any())
+        {
+            var pages = new Page[]
+            {
+                new Page
+                {
+                    Title = "About Us",
+                    Content = "Welcome to our About Us page...",
+                    IsActive = true
+                },
+                new Page
+                {
+                    Title = "Contact Us",
+                    Content = "Feel free to contact us anytime...",
+                    IsActive = true
+                },
+			    // Add more Page entries as needed
+		    };
 
-	public class DbSeeder
-	{
-	public static void Seed(AppDbContext dbContext)
-	{
-		// Check if Pages already exist in the database
-		if (dbContext.Pages.Any())
-		{
-			return; // Database has been seeded
-		}
-
-		var pages = new Page[]
-		{
-				new Page
-				{
-					Title = "About Us",
-					Content = "Welcome to our About Us page...",
-					IsActive = true
-				},
-				new Page
-				{
-					Title = "Contact Us",
-					Content = "Feel free to contact us anytime...",
-					IsActive = true
-				},
-			// Add more Page entries as needed
-		};
-
-		dbContext.Pages.AddRange(pages);
-		dbContext.SaveChanges();
-	}
+            dbContext.Pages.AddRange(pages);
+            dbContext.SaveChanges();
+        }
+    }
 
 
 }
--- Ads.Data/Entity/Category.cs
+++ Ads.Data/Entity/Category.cs
@@ -2,14 +2,19 @@
 
 namespace Ads.Data.Entity
 {
-	public class Category
-	{
-		[Key]
-		public int? Id { get; set; }
+    public class Category
+    {
+        [Key]
+        public int? Id { get; set; }
 
-		[MaxLength(100)]
-		public string? Name { get; set; }
-		[MaxLength(200)]
-		public string? Description { get; set; }
-	}
+        [MaxLength(100)]
+        public string? Name { get; set; }
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        //public List<Advert> Adverts { get; set; }
+
+
+        public List<CategoryAdvert> CategoryAdverts { get; set; }
+    }
 }
